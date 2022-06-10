fda2498eda7d176fb2629e7268240f28192b308b
PanyaOu <75552913+PanyaOu@users.noreply.github.com>
2022-06-09 17:28:45 -0700

    prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
-import './css/App.css'
+import "./css/App.css";
 export default function App() {
   return (
     <div>
       <h1>Panya's Portfolio</h1>
       <nav>
-        <Link className="link" to="/sorting">Sorting Algorithms</Link>
-        <Link className="link" to="/pathfinding">PathFinding Algorithms</Link>
+        <Link className="link" to="/sorting">
+          Sorting Algorithms
+        </Link>
+        <Link className="link" to="/pathfinding">
+          PathFinding Algorithms
+        </Link>
       </nav>
     </div>
   );
